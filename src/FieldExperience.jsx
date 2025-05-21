99ad858ac0fe027f6781b91477b219c8498df971
Percival <95fjpl95@gmail.com>
2025-05-21 06:06:36 -0600

    feat: FieldExperience entries ready

--- src/FieldExperience.jsx
+++ src/FieldExperience.jsx
@@ -2,6 +2,9 @@ import PropTypes from 'prop-types';
 import {useHandler} from './Lib.js'
 import Input from './Input.jsx'
 import InputOkClear from './InputOkClear.jsx'
+import css from './FieldExperience.module.css'
+import trash from './assets/trash.svg'
+
 export default function FieldExperience({ getter, setter }){
   const initial = {where:"", when:"", what:"", desc:""}
   const {newEntry, 
@@ -22,12 +25,14 @@ export default function FieldExperience({ getter, setter }){
 
       {
         getter.experience.map(({when, where, what, desc}, indx) => 
-          <div key={indx}>
-            <Input index={indx} name="when"  type="text" value={when}  onChange={entryEdit} /> 
-            <Input index={indx} name="where" type="text" value={where} onChange={entryEdit} /> 
-            <Input index={indx} name="what"  type="text" value={what}  onChange={entryEdit} /> 
-            <Input index={indx} name="desc"  type="text" value={desc}  onChange={entryEdit} /> 
-            <button data-index={indx} onClick={entryDelete}>x</button>
+          <div key={indx} className={css.entries}>
+            <div className={css.inputs}>
+              <Input index={indx} name="when"  type="text" value={when}  onChange={entryEdit} /> 
+              <Input index={indx} name="where" type="text" value={where} onChange={entryEdit} /> 
+              <Input index={indx} name="what"  type="text" value={what}  onChange={entryEdit} /> 
+              <Input index={indx} name="desc"  type="text" value={desc}  onChange={entryEdit} /> 
+            </div>
+            <button data-index={indx} onClick={entryDelete} className={css.remove}><img src={trash}/></button>
           </div>)
       }
     </>
