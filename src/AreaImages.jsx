59afc128394f95bf8d33a39316b3dd2b552cef7d
Percival <95fjpl95@gmail.com>
2025-05-30 12:17:28 -0600

    fix: improved responsiveness, changed image-toggle icon

--- src/AreaImages.jsx
+++ src/AreaImages.jsx
@@ -1,12 +1,16 @@
 import PropTypes from 'prop-types';
 import {useState} from 'react'
 import css from './AreaImages.module.css'
+//import on from './assets/on.svg'
+//import off from './assets/off.svg'
+import img_on from './assets/img_on.svg'
+import img_off from './assets/img_off.svg'
 export default function AreaImages({getter}){
   const [visibility, setVisibility] = useState(false)
   const toggle = () => setVisibility(()=> !visibility)
 
   return <>
-    <button onClick={toggle} className={css.toggle}>show</button>
+    <button onClick={toggle} className={css.toggle}> <img src={visibility? img_on : img_off} /> </button>
     { visibility &&
       getter.education.map(({institution, title, url, image}, indx) => <div key={indx} className={css.card}>
       <figure>
